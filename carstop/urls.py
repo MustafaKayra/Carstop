4f27f36b233a2f8f90828a57278e5e5d03a7edcc
MustafaKayra <m.toker2009@gmail.com>
2025-06-17 15:37:29 +0300

    Kullanıcı kayıt olma sayfası eklendi

--- carstop/urls.py
+++ carstop/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from shop import views
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="index"),
     path('publishcar/',views.adpost,name="adpost"),
-    path('ad/<slug:slug>',views.adetail,name="adetail")
+    path('ad/<slug:slug>',views.adetail,name="adetail"),
+    path('createuser/',users_views.createuser,name="createuser")
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login,authenticate,logout
+from .forms import UserForm
 
-# Create your views here.
+def createuser(request):
+    if request.method == "POST":
+        form = UserForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            print(f"Yeni Kullanıcı: {user}")
+            return redirect('index')
+        else:
+            print(form.errors)
+    else:
+        form = UserForm()
+    return render(request,"createuser.html",{"form": form})
+
