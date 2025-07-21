463142890470aa42595b72cc4b79043e94265b26
MustafaKayra <m.toker2009@gmail.com>
2025-07-21 16:53:30 +0300

    requirements.txt düzenlendi

--- carstop/settings.py
+++ carstop/settings.py
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary_storage',
-    'cloudinary'
+    'cloudinary',
     'shop',
-    'users'
+    'users',
 ]
 
 MIDDLEWARE = [
